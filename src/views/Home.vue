5c9814fc7910ae6c7e33298d16edaae7a0f29737
unknown <d.news@hotmail.fr>
2023-10-24 12:55:31 +0200

    config app

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-col p-8 items-center justify-center">
+      <input
+        type="text"
+        class="rounded border-2 border-gray-200 w-full"
+        placeholder="Search for Meals"
+      />
+      <div class="flex gap-1">
+        <router-link to="/" v-for="letter of letters" :key="letter">
+        {{ letter }}
+        </router-link>
+      </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import store from "../store";
+
+const meals = computed(() => store.state.meals);
+const letters = 'ABCDEFGHIKLMNOPQRSTVWXYZ'.split("");
+</script>
